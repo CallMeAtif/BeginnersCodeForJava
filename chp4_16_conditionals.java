2a5b5c7d6da208db54b5af3953faef2c9d6707ee
Atiful <Atif472005@gmail.com>
2022-02-11 00:19:24 +0530

    initial commit

---
+++ chp4_16_conditionals.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class chp4_16_conditionals {
+    public static void main(String[] args) {
+        int a = 18;
+        if(a>=18)
+            System.out.println("He can drive");
+        else
+            System.out.println("he cant drive");
+
+    }
+}
