a841b02e519f1f9baf5bb8eac49f79d08e179599
Atiful <Atif472005@gmail.com>
2022-02-10 23:13:40 +0530

    intial commit

---
+++ ch3_15_exercise_3.java
@@ -0,0 +1,39 @@
+package com.company;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ch3_15_exercise_3 {
+    public static void main(String[] args) {
+
+        //Question 1
+
+        Scanner sc = new Scanner(System.in);
+//        String sen = sc.nextLine();
+//        System.out.println(sen.toLowerCase());
+
+        //Question 2
+
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.nextLine();
+//        System.out.println(name.replace(" ","_"));
+
+        //Question 3
+
+//        System.out.println("Enter You name:");
+//        Scanner sc = new Scanner(System.in);
+//        String name = sc.next();
+//        String letter = "Dear <|Name|>,Thanks a lot";
+//        System.out.println(letter.replace("<|Name|>",name));
+
+        //Question 4
+
+//        String a = sc.nextLine();
+//        System.out.println(a.indexOf("  "));
+//        System.out.println(a.indexOf("   "));
+
+        //Question 5
+
+        String letter = "Dear Harry, \nThis Course is nice Thanks!";
+        System.out.println(letter);
+    }
+}
