addacf74d82c34f8a26e42f71569e0dead363d82
Atiful <Atif472005@gmail.com>
2022-02-12 21:57:50 +0530

    inital commit

---
+++ chp5_21_whileloops.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+
+public class chp5_21_whileloops {
+    public static void main(String[] args) {
+        System.out.println("Enter A Number: ");
+        Scanner sc = new Scanner(System.in);
+        byte i = 1;
+        byte b = sc.nextByte();
+        while(i<=10){
+            byte a = (byte) (i*b);
+            System.out.printf("%d X %d = %d",b,i,a);
+            System.out.print("\n");
+            i++;
+        }
+    }
+}
